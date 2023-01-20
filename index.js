925b6f8d3d3308e3c74998bae26640921bb89849
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-20 15:53:55 +0000

    managed to access the current hour of the time value and accessed the timedata attri in the div

--- index.js
+++ index.js
@@ -8,3 +8,33 @@ $("#currentDay").text(today.format("dddd, MMMM Do"));
 //Event is grey when the current time is passed.
 //Event is red when the time is current.
 //Event is green which in the future.
+
+
+//pesudo code - compare current time to the time value in the span id current time, if it's higher than the number then it should go grey, if its equal then its green and if its higher then its red, we compare the first value HH do not include mins
+
+    // get the current time
+    // how to take the hours from the current time
+    // compare it 
+    // display the right color
+
+let currentTime = moment();
+
+let doo = $("#currentTime").text(currentTime.format("HH"));
+
+let ooo = currentTime.format("HH");
+
+console.log($("#timeSlot").attr('timedata'));
+
+let timeSlot = $("#timeSlot").attr('timedata')
+
+function compareCalendarTimeAndCurrentTime () {
+    console.log(ooo);
+    console.log(timeSlot);
+
+    if(ooo == timeSlot){
+        $('#event9').css('background-color', 'red');
+    }
+
+}
+
+compareCalendarTimeAndCurrentTime();
