5c6bf59d738ebb1da2d5bc7d18867fd0b5f24673
Vinh La <kietla92@Vinhs-MacBook-Pro.local>
2023-01-22 20:20:37 +0000

    tweaking the for loop function so that it applies the correct class, the correct class was not being applied as it was applying string to numbers which has been addressed by using parseInt on the currentHour variable

--- index.js
+++ index.js
@@ -9,88 +9,80 @@ let currentTime = moment();
 
 //currentHourTime
 
-let currentHour = currentTime.format("HH");
-
+let currentHour = parseInt(currentTime.format("HH"));
 
 //Gets all the timedata attributes in the html.
 
 let timeSlots = document.querySelectorAll("[timedata]");
 
-// This maps through the timeslots attributes and returns the values and stores them in an array to be used in a for loop. 
+// This maps through the timeslots attributes and returns the values and stores them in an array to be used in a for loop.
 
-let timedataArray = Array.from(timeSlots).map(function(timeSlot) {
+let timedataArray = Array.from(timeSlots).map(function (timeSlot) {
   return timeSlot.getAttribute("timedata");
 });
 
 // console.log(timedataArray);
 
+for (let i = 0; i < timedataArray.length; i++) {
+  let time = timedataArray[i];
+  let currentDiv = $(`[timedata=${time}]`).closest("div"); //The .clostest finds the cloest ancestor div element of the selected element.
+  // let testDiv2 = $('[timedata');//This selects all timedata attrbutes and not the ones that specficially match the hour.
+  let eventContainer = currentDiv.siblings("#eventContainer"); //The siblings method will select the div element with the ID "eventContainer" and will apply the css class based upon the conditions below.
 
-for(let i=0; i<timedataArray.length; i++) {
-    let time = timedataArray[i];
-    let currentDiv = $(`[timedata=${time}]`).closest('div'); //The .clostest finds the cloest ancestor div element of the seelcted element.
-         // let testDiv2 = $('[timedata');//This selects all timedata attrbutes and not the ones that specficially match the hour.
-
-    let eventContainer = currentDiv.siblings('#eventContainer');//The siblings method will select the div element with the ID "event9" and will apply the css class based upon the conditions below.
-    console.log(currentHour);
-    console.log(time);//timedata attr arrays
-    if (time === currentHour) {
-        $(eventContainer).addClass('present');
-    } else if (currentHour < time) {
-        $(eventContainer).addClass('future');
-
-    } else if (currentHour > time) {
-        $(eventContainer).addClass('past');
-    }
-};
-
+  if (currentHour === time) {
+    $(eventContainer).addClass("present");
+  } else if (currentHour > time) {
+    $(eventContainer).addClass("past");
+  } else if (currentHour < time) {
+    $(eventContainer).addClass("future");
+  }
+}
 
 function saveBtnText(event) {
-    event.preventDefault();
-    let currentSaveBtn = event.target;
+  event.preventDefault();
+  let currentSaveBtn = event.target;
 
-    $(currentSaveBtn).closest('text-area');
-
-    console.log($(currentSaveBtn).closest('text-area'));
-
+  $(currentSaveBtn).closest("text-area");
 }
 
 let saveBtn = document.querySelectorAll(".saveBtn");
 
 $(".saveBtn").on("click", function (event) {
-    event.preventDefault();
-    let currentSaveBtn = $(event.target);//This targets the button element and stores it in a variable.
+  event.preventDefault();
+  let currentSaveBtn = $(event.target); //This targets the button element and stores it in a variable.
 
-    let textAreaValue = currentSaveBtn.closest('div.row').find('textarea#eventText').val();//This methods finds the closest div with the class row and .find, finds the textarea element with the id "eventText" and retrieves the value.
+  let textAreaValue = currentSaveBtn
+    .closest("div.row")
+    .find("textarea#eventText")
+    .val(); //This methods finds the closest div with the class row and .find, finds the textarea element with the id "eventText" and retrieves the value.
 
-    let timedataValue = currentSaveBtn.closest('div.row').find('#timeSlot').attr('timedata'); //This method finds the cloest div with the class row and .find, finds an element with the ID timeSlot with the attribute timedata and returns the values.
+  let timedataValue = currentSaveBtn
+    .closest("div.row")
+    .find("#timeSlot")
+    .attr("timedata"); //This method finds the cloest div with the class row and .find, finds an element with the ID timeSlot with the attribute timedata and returns the values.
 
-    let textTimeEntry = localStorage.setItem(timedataValue, textAreaValue);
-
+  let textTimeEntry = localStorage.setItem(timedataValue, textAreaValue);
 });
 
-$(document).ready(function() {
-    let eventText = document.querySelectorAll("#eventText");
-      for (let i = 0; i < timedataArray.length; i++) {
-        let time = timedataArray[i];
+$(document).ready(function () {
+  let eventText = document.querySelectorAll("#eventText");
+  for (let i = 0; i < timedataArray.length; i++) {
+    let time = timedataArray[i];
 
-        let storedValue = localStorage.getItem(time);
+    let storedValue = localStorage.getItem(time);
 
-        console.log(storedValue);
-        $(eventText[i]).val(storedValue);
-
-      }
-  });
+    $(eventText[i]).val(storedValue);
+  }
+});
 
 //pseudocode//
 
 //Should I disable save button or change it to an edit button and vice-versa?
 
-
-
 // function showEvents() {
 
 //     console.log(timedataArray);
-    
+
 //     for (let i = 0; i < timedataArray.length; i++) {
 //         let time = timedataArray[i];
 //         let storedEvent = localStorage.getItem(time);
@@ -98,7 +90,6 @@ $(document).ready(function() {
 //         $('#eventText').text(storedEvent);
 
 //     }
-
 
 //     // let testArray = ["30"];
 //     // let storedEvent = localStorage.getItem(testArray);
@@ -109,9 +100,7 @@ $(document).ready(function() {
 
 // showEvents();
 
-
 //pseudocode//
-
 
 //when user clicks on the events container, it should allow them to enter a text//[done]
 
@@ -125,30 +114,24 @@ $(document).ready(function() {
 
 //possibly include a delete or edit feature?//
 
-
 //Present timeblocks based on the current time the page is viewed //
 
 //Event is grey when the current time is passed.
 //Event is red when the time is current.
 //Event is green which in the future.
 
-
 //pesudo code - compare current time to the time value in the span id current time, if it's higher than the number then it should go grey, if its equal then its green and if its higher then its red, we compare the first value HH do not include mins
 
-    // get the current time
-    // how to take the hours from the current time
-    // compare it 
-    // display the right color
+// get the current time
+// how to take the hours from the current time
+// compare it
+// display the right color
 
-    //manually solve it for each manually//
+//manually solve it for each manually//
 
-//save the input value to local storage for each event 
+//save the input value to local storage for each event
 
 //data-time for each button
-
-
-
-
 
 // console.log($("#timeSlot").attr('timedata'));
 
